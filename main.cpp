45aad399d1ac5fa04f25f4ca26f03fd5ea72c163
white0nrice <bryanjoe421@gmail.com>
2017-02-21 02:49:24 -0800

    Added a To-Do List in main.cpp
    added To-Do list, can separate into separate file if necessary

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
+/* To Do: 
+	Read list files
+	Create Vector for Item names
+	Check prices on each list file and find average
+	put averages into separate vector
+	print vectors*/
+
 #include <iostream>
+#include <vector>
+#include <fstream>
 using namespace std;
 
 int main(int argc, char *argv[]) {
+	int size = argc;
+	//Creates a vector containing all item names
+	for (int i = 1; i < size; i++) {
+
+	}
 
 
     return 0;
