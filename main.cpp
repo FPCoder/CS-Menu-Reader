86d45450b586c69e01c6764eb197e4722037a0b5
white0nrice <bryanjoe421@gmail.com>
2017-02-21 16:23:56 -0800

    Finished Part 1
    Wrote Part 1 of project and added sections for part 2 and 3. Will upload
    .txt file for psuedo later tonight

--- main.cpp
+++ main.cpp
@@ -8,14 +8,33 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <string>
 using namespace std;
 
 int main(int argc, char *argv[]) {
 	int size = argc;
-	//Creates a vector containing all item names
-	for (int i = 1; i < size; i++) {
-
+	std::string str;
+	std::vector<double> prices;
+	std::vector<string> names;
+	std::vector<int> recurrence;
+	//Creates a vector containing item names of 1st menu
+	string name = "";
+	double num;
+	ifstream fileReader;
+	fileReader.open(str);
+	while (!(fileReader.eof())) {
+		fileReader >> name;
+		names.push_back(name);
+		fileReader >> num;
+		prices.push_back(num);
+		recurrence.push_back(1);
 	}
+	//Checks subsequent menues with vectors and adds to items/prices/recurrence accordingly
+	//TO-DO
+
+
+	//Divides prices by corresponding recurrence and prints accordingly
+	//TO-DO
 
 
     return 0;
